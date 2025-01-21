43e8053973cf857107f87c9988bd0752b4bf4737
indumatic <electrico@indumatic.net>
2025-01-21 13:53:33 +0100

    added event listeners

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const container = document.getElementById("container")
 
+const createElementWithIdAndClasses = (element) => (id, ...classes) => {
+    const element = document.createElement(element)
+    element.id = id
+    for (cl in classes) {
+        element.classList.add(cl)
+    }
+    return element
+}
+
+const createDiv = createElementWithIdAndClasses("div")
+const createButton = createElementWithIdAndClasses("button")
+
 for(let i = 1; i<6; i++) {
     let div = document.createElement("div")
-    div.classList.add("card",`level${i}`,"selected")
-    div.id = `card-${i}`    
+    div.classList.add("card",`level${i}`)
+    div.id = `card-${i}`
+    
+    //let div = createDiv(`card-${i}`,"card",`level${i}`,"selected")
 
     let text = document.createElement("div")
     text.classList.add("text")
@@ -14,16 +28,25 @@ for(let i = 1; i<6; i++) {
     button.classList.add("del", "show")
     button.id = `del-card-${i}`
     button.textContent = "del"
-
-    
+     
     div.appendChild(button)
     div.appendChild(text)
+
+    div.addEventListener("click", (e) => {
+        Array.from(container.querySelectorAll(".card"))
+            .forEach(card => card.classList.remove("selected"))
+        e.currentTarget.classList.add("selected")
+
+        if(e.target.classList.contains("del")) alert("del")
+    })
+
+
     container.appendChild(div)
 }
 
 for(let i = 1; i<6; i++) {
     let div = document.createElement("div")
-    div.classList.add("card", "description",`level${i}`,"selected")
+    div.classList.add("card", "description",`level${i}`)
     div.id = `card-description-${i}`    
 
     let title = document.createElement("div")
@@ -46,3 +69,4 @@ for(let i = 1; i<6; i++) {
     div.appendChild(description)
     container.appendChild(div)
 }
+
